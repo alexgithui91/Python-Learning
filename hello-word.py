3235f485bab6e1fda071a65aea56bc0b1ccebafc
alexgithui91 <alexgithui91@gmail.com>
2021-03-28 20:10:08 +0300

    ...

--- hello-word.py
+++ hello-word.py
@@ -3,3 +3,6 @@ print("Hello Welcome to Python3")
 userName = input("Enter your name: ")
 
 print(userName)
+
+#git config --global user.email "you@example.com"
+#git config --global user.name "Your Name"
