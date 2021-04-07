85a3ab1f63585f713b3745a937c71740bb1533fa
alexgithui91 <alexgithui91@gmail.com>
2021-04-07 17:25:32 +0300

    fixed formatting

--- storeFiles.py
+++ storeFiles.py
@@ -11,7 +11,7 @@ filesDir = os.path.join(BASE_DIR, "templates")
 
 os.makedirs(filesDir, exist_ok=True)
 
-myFiles = range(0,12)
+myFiles = range(0, 12)
 
 for i in myFiles:
     fileName = f"{i}.txt"
@@ -19,6 +19,6 @@ for i in myFiles:
     if os.path.exists(filePath):
         print(f"Skipped {fileName} Already exists")
         continue
-    
+
     with open(filePath, 'w') as f:
-        f.write(i * "Hey hey hey!!!\n") 
+        f.write(i * "Hey hey hey!!!\n")
