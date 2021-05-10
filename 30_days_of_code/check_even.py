b48a0340f1d4d2dc640343eac0f30321be1fe99e
alexgithui91 <alexgithui91@gmail.com>
2021-05-10 20:24:47 +0300

    day 3 check even

--- 30_days_of_code/check_even.py
+++ 30_days_of_code/check_even.py
@@ -10,3 +10,13 @@ import sys
 
 if __name__ == '__main__':
     N = int(input().strip())
+
+    if N % 2 == 0:
+        if N in range(2, 5):
+            print("Not Weird")
+        elif N in range(6, 21):
+            print("Weird")
+        elif N >= 20:
+            print("Not Weird")
+    else:
+        print("Weird")
