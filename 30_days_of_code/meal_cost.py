a5d1d49a50e4a3e402495622d71dc89a6b077895
alexgithui91 <alexgithui91@gmail.com>
2021-05-10 20:15:28 +0300

    a bit of formatting

--- 30_days_of_code/meal_cost.py
+++ 30_days_of_code/meal_cost.py
@@ -16,13 +16,15 @@ import sys
 #  3. INTEGER tax_percent
 #
 
+
 def solve(meal_cost, tip_percent, tax_percent):
     # Write your code here
-    tip = meal_cost * (tip_percent/100)
-    tax = meal_cost * (tax_percent/100)
+    tip = meal_cost * (tip_percent / 100)
+    tax = meal_cost * (tax_percent / 100)
     total_cost = meal_cost + tip + tax
-    
+
     return(round(total_cost))
+
 
 if __name__ == '__main__':
     meal_cost = float(input("Enter meal cost : ").strip())
