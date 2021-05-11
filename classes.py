83f0f1f79ec9c04940ae85f6567a04a00cb537d2
alexgithui91 <alexgithui91@gmail.com>
2021-05-11 09:36:44 +0300

    working with classes

--- classes.py
+++ classes.py
@@ -8,17 +8,30 @@ class Dog():
         # Instance attributes
         self.name = name
         self.age = age
+        # self.breed = breed
 
     # Instance method
     def __str__(self):
-        return f"{self.name} is {self.age} years old! And is {self.species}"
+        return f"{self.name} is {self.age} years old! And is species {self.species}"
 
     # Another Instance method
     def speak(self, sound):
-        return f"{self.name} says {sound}"
+        return f"{self.name} barks : {sound}!!!"
 
+class JackRussellTerrier(Dog):
+    def speak(self, sound="Arf"):
+        # return f"{self.name} says {sound}"
+        return super().speak(sound)
 
-boscoe = Dog("Boscoe", 4)
+class Dachshund(Dog):
+    pass
+
+class Bulldog(Dog):
+    pass
+
+boscoe = JackRussellTerrier("Boscoe", 3)
 print(boscoe)
-# print(boscoe.description())
-print(boscoe.speak("Woof Woooof"))
+print(boscoe.speak())
+# rex = Bulldog("Rex", 4)
+# print(rex)
+# print(rex.speak("hey"))
