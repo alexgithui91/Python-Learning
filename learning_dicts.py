517bceaf8503c8b3202d01040c1b3680721c0f53
alexgithui91 <alexgithui91@gmail.com>
2021-05-11 21:20:02 +0300

    beginner dicts

--- learning_dicts.py
+++ learning_dicts.py
@@ -26,7 +26,9 @@ mlb_teams = dict(
 )
 
 # Print dict item
-print(basketball_teams["Houston"])
+# print(basketball_teams["Houston"])
+print(basketball_teams.get("Dallas"))
+
 
 # Add to dict
 cars["Mazda"] = "Demio" 
@@ -35,3 +37,24 @@ print(cars)
 # Delete an item from dict
 del mlb_teams["Seattle"]
 print(mlb_teams)
+
+# Clear dict
+cars.clear()
+print(cars)
+
+print(cars.items())
+
+print(cars.keys())
+
+print(basketball_teams.values())
+
+basketball_teams.pop("Detroit")
+print(basketball_teams)
+
+basketball_teams.popitem()
+print(basketball_teams)
+
+
+basketball_teams.update(mlb_teams)
+
+print(basketball_teams)
