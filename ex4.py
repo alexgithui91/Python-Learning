8457e79fbe145f0dd55b8b6b4356f5b8c495c890
alexgithui91 <alexgithui91@gmail.com>
2021-06-07 21:13:36 +0300

    ex4 working with variables

---
+++ ex4.py
@@ -0,0 +1,15 @@
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_car
+average_passengers_per_car = passengers / cars_driven
+
+print("Cars available", cars)
+print("Drivers available", drivers)
+print("Empty cars", cars_not_driven)
+print("Transport available", carpool_capacity)
+print("Passengers", passengers)
+print("Passengers in each car", average_passengers_per_car)
