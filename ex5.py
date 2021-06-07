2ce2ef31c2cdc39bdbd18799590b4fdbe365ca76
alexgithui91 <alexgithui91@gmail.com>
2021-06-07 22:25:31 +0300

    more complex-ish variable work

---
+++ ex5.py
@@ -0,0 +1,11 @@
+my_name = "Alex"
+my_age = 29
+my_height = 84  # inches
+my_weight = 75  # kgs
+my_eyes = "black"
+my_teeth = "white"
+my_hair = "black"
+
+print("Hi. My name is %s" % my_name)
+print("I am %d years young :-)" % my_age)
+print("I am about %d inches tall and %d kgs" % (my_height, my_weight))
