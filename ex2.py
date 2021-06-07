e16b3937cc4f56b3ff2f2520eac9b84f1073704a
alexgithui91 <alexgithui91@gmail.com>
2021-06-07 14:22:08 +0300

    working with comments

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# A comment, this is so you can read your program later.
+# Anything after # is ignored by Python
+
+print("I can code this")  # And the comment after is ignored
+# Comments can also be used to disable parts of code like below
+# print("I do not want this to run")
+
+print("But I do want this to run!!")
+
+print("this sucks")
