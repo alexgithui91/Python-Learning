923ed3c0146f9cc46908c62fe692a9c3657c8c18
alexgithui91 <alexgithui91@gmail.com>
2021-07-09 21:08:35 +0300

    ex8.py

--- ex8.py
+++ ex8.py
@@ -1,3 +1,12 @@
 formatter = "%r %r %r %r"
 
 print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % (True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
+print(
+    formatter %
+    ("This is test statement 1",
+     "This is test statement 2",
+     "This is test statement 3",
+     "This is test statement 4"))
