051e25445fbb793c15722d1b3e947ef3fde5d167
alexgithui91 <alexgithui91@gmail.com>
2021-07-21 22:02:44 +0300

    ex19.py

---
+++ ex19.py
@@ -0,0 +1,15 @@
+def gold_and_Silver(gold_count, silver_count):
+    print("Take a few bricks of %s" % gold_count)
+    print("And the take a few bars of %s" % silver_count)
+
+
+# Method 1
+gold_and_Silver(18, 39)
+
+# Method2
+gold = 50
+silver = 100
+gold_and_Silver(gold, silver)
+
+# Method 3
+gold_and_Silver(10+9, 15+10)
